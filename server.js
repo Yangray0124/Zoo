0d7d94017ec4c5e41cd9b974a503d593384b45e6
Yangray0124 <yangray0124@gmail.com>
2026-01-16 04:32:32 +0800

    port

--- server.js
+++ server.js
@@ -608,5 +608,5 @@ function broadcastGameState(room) {
     });
 }
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
